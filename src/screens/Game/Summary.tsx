86dcb9eba3445af831031ab0250c8a703e2f03b7
Henzii <80477931+Henzii@users.noreply.github.com>
2022-03-07 14:54:56 +0200

    Varia tuloksiin, kallistus kaanto jne.

--- src/screens/Game/Summary.tsx
+++ src/screens/Game/Summary.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { useSelector } from 'react-redux';
-import useGame from '../../hooks/useGame';
+import useGame, { Scorecard } from '../../hooks/useGame';
 import { gameData } from '../../reducers/gameDataReducer';
 import { RootState } from '../../utils/store';
 import Container from '../../components/ThemedComponents/Container';
-import { Table, Rows, Row } from 'react-native-table-component';
+import { Table, Row, Cell, Cols, TableWrapper } from 'react-native-table-component';
 import Loading from '../../components/Loading';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 const Summary = () => {
     const gameData = useSelector((state: RootState) => state.gameData) as gameData;
@@ -17,37 +18,94 @@ const Summary = () => {
         );
     }
     const sortedScorecards = [...data.scorecards].sort((a, b) => (a.total || 0) - (b.total || 0));
-    const tableHeaders = ['#', 'Player', ...data.pars.map((p, i) => i + 1), 'Total', '+/-', 'Hc', 'bHc', 'hcTot', 'Hc+/-'];
-    const playersData = sortedScorecards.map((sc, i) => {
-        const scoret: (number | string)[] = [...sc.scores];
-        // Tehdään tekemättömistä väylistä tyhjiä...
-        for (let i = 0; i < data.holes; i++) {
-            if (!scoret[i]) scoret[i] = ' ';
-        }
-        return [i + 1,
-        sc.user.name,
-        ...scoret,
-        sc.total || '?',
-        sc.plusminus,
-        sc.hc,
-        (sc.beers/2),
-        (sc.total || 0) - (sc.beers/2) - (sc.hc),
-        (sc.plusminus || 0) - (sc.beers/2) - sc.hc || '?',
-        ];
-    });
-    const leveydet = [20, 80, ...data.pars.map(() => 30), 50, 50, 50, 50, 50, 50];
+    const tableHeaders = [...data.pars.map((p, i) => i + 1), 'Total', '+/-', 'Hc', 'bHc', 'hcTot', 'Hc+/-'];
+    const leveydet = [...data.pars.map(() => 30), 50, 50, 50, 50, 50, 50];
+    const nimetJaSijoitukset = sortedScorecards.reduce((p: [string[], string[]], c, i) => {
+        p[0].push((i+1)+'');
+        p[1].push(c.user.name);
+        return p;
+    }, [[],[]]);
     return (
-        <ScrollView horizontal>
-            <Container>
+        <Container noPadding style={{ flexDirection: 'row' }}>
+            <View style={{ width: 110 }}>
+                <Table>
+                    <Row data={['#', 'Player']} widthArr={[30, 80]} style={tyylit.header} textStyle={tyylit.headerText} />
+                    <Cols data={nimetJaSijoitukset} widthArr={[30,80]} textStyle={[tyylit.text, tyylit.scoreCell]}  />
+                </Table>
+            </View>
+            <ScrollView horizontal>
                 <Table>
                     <Row data={tableHeaders} style={tyylit.header} textStyle={tyylit.headerText} widthArr={leveydet} />
-                    <Rows data={playersData} textStyle={tyylit.text} style={tyylit.rivit} widthArr={leveydet} />
+                    {sortedScorecards.map((sc, i) => (
+                        <SinglePlayerDataRow
+                            key={sc.user.id + 'scData'}
+                            position={i + 1}
+                            pars={data.pars}
+                            scorecard={sc} />)
+                    )}
                 </Table>
-            </Container>
-        </ScrollView>
+            </ScrollView>
+        </Container>
+    );
+};
+const SinglePlayerDataRow = ({ scorecard, position, pars }: { scorecard: Scorecard, position: number, pars: number[] }) => {
+    const pickColor = (par: number, score: number) => {
+        switch (par - score) {
+            case 0:
+                return tyylit.par;
+            case 1:
+                return tyylit.birdie;
+            case -1:
+                return tyylit.bogie;
+            default:
+                if ((par - score < -1)) return tyylit.doubleBogie;
+                else if (par - score > 1) return tyylit.eagle;
+        }
+    };
+    const dataCells = pars.map((p, i) => {
+        return (<Cell
+            key={scorecard.user.name + i + 'score'}
+            data={scorecard.scores[i] || ' '}
+            textStyle={tyylit.text}
+            style={[tyylit.scoreCell, pickColor(p, scorecard.scores[i])]}
+            width={30}
+        />);
+    });
+    return (
+        <TableWrapper style={tyylit.rivi}>
+            {dataCells}
+            <Cell data={scorecard.total} width={50} textStyle={tyylit.text} />
+            <Cell data={scorecard.plusminus} width={50} textStyle={tyylit.text} />
+            <Cell data={scorecard.hc} width={50} textStyle={tyylit.text} />
+            <Cell data={scorecard.beers / 2} width={50} textStyle={tyylit.text} />
+            <Cell data={((scorecard.total || 0) - scorecard.hc - scorecard.beers / 2)} width={50} textStyle={tyylit.text} />
+            <Cell data={(scorecard.plusminus || 0) - scorecard.hc - scorecard.beers / 2} width={50} textStyle={tyylit.text} />
+        </TableWrapper>
     );
 };
 const tyylit = StyleSheet.create({
+    scoreCell: {
+        borderRadius: 20,
+        marginTop: 3,
+    },
+    doubleBogie: {
+        backgroundColor: '#fcbc9a',
+    },
+    bogie: {
+        backgroundColor: '#fce79a',
+    },
+    eagle: {
+        backgroundColor: 'rgb(255,207,64)'
+    },
+    birdie: {
+        backgroundColor: '#aadaf2',
+    },
+    par: {
+        backgroundColor: '#50857255'
+    },
+    rivi: {
+        flexDirection: 'row',
+    },
     header: {
         backgroundColor: 'rgb(90,200,90)',
     },
