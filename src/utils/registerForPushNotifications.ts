d8c55680222b88409d8b707e6adfb061b919e15b
Henzii <80477931+Henzii@users.noreply.github.com>
2022-03-10 21:44:21 +0200

    Expo push notifikaatioiden valmistelua

---
+++ src/utils/registerForPushNotifications.ts
@@ -0,0 +1,34 @@
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+import { Platform } from 'react-native';
+
+const registerForPushNotificationsAsync = async (): Promise<string | undefined> => {
+    let token: string | undefined;
+    if (Device.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+    } else {
+        alert('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        });
+    }
+    return token;
+};
+
+export default registerForPushNotificationsAsync;
