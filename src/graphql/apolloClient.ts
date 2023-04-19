7eb8b202549691c9444e1ceec4a5752f0eff30c0
Henzii <80477931+Henzii@users.noreply.github.com>
2023-04-19 19:54:25 +0300

    add wsLink

--- src/graphql/apolloClient.ts
+++ src/graphql/apolloClient.ts
@@ -1,10 +1,15 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { ApolloClient, InMemoryCache, ApolloLink, HttpLink } from '@apollo/client';
-import { setContext } from 'apollo-link-context';
+import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 const PRODUCTION_URI = 'https://fudisc-server.henzi.fi';
 const PREVIEW_URI = 'https://fudisc-dev.herokuapp.com/graphql';
-const DEVELOPMENT_URI = 'http://192.168.1.4:8080/graphql';
+const DEVELOPMENT_URI = 'http://192.168.1.5:8080/graphql';
+
+const SUB_URL = 'ws://192.168.1.5:8080/graphql';
 
 export const getAPIUrl = async () => {
     const localMode = await AsyncStorage.getItem('apiEnv') || process.env.NODE_ENV;
@@ -18,6 +23,16 @@ export const getAPIUrl = async () => {
 };
 
 const httpLink = new HttpLink();
+
+const wsLink = new GraphQLWsLink(createClient({
+    url: SUB_URL,
+    connectionParams: async () => {
+        const token = await AsyncStorage.getItem('token');
+        return {
+            Authorization: token ? `bearer ${token}` : ''
+        };
+    },
+}));
 
 const authLink = setContext( async (_root: unknown, { headers }) => {
     const token = await AsyncStorage.getItem('token');
@@ -33,8 +48,20 @@ const authLink = setContext( async (_root: unknown, { headers }) => {
     };
 });
 
+const splitLink = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      );
+    },
+    wsLink,
+    authLink.concat(httpLink),
+  );
+
 export const client = new ApolloClient({
-    link: (authLink.concat(httpLink as never) as unknown as ApolloLink),
+    link: splitLink,
     cache: new InMemoryCache(),
 });
 
