3edd7cf69273fa0c02253d8a3c09d239b09bb924
Karlenius Henry <henry.karlenius@veikkaus.fi>
2025-08-28 08:41:34 +0300

    add i18n, no tee sign image, HoleSelector

--- src/screens/Game/HoleMap/index.tsx
+++ src/screens/Game/HoleMap/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import type { FC } from "react";
-import { View } from "react-native";
+import { Image, StyleSheet, View, Dimensions } from "react-native";
 import { Button, Text } from 'react-native-paper';
 import * as ExpoImagePicker from 'expo-image-picker';
 import { useGameStore } from 'src/zustand/gameStore';
 import { useHoleMapUpload } from '@hooks/useHoleMapUpload';
+import { useTranslation } from 'react-i18next';
+import Container from '@components/ThemedComponents/Container';
+import Spacer from '@components/ThemedComponents/Spacer';
+import HoleSelector from '@components/HoleSelector';
 
 const HoleMap: FC = () => {
+    const {t} = useTranslation();
 
-    const selectedRound = useGameStore(state => state.selectedRound);
+    const [selectedRound, setSelectedRound] = useGameStore(state => [state.selectedRound, state.setSelectedRound]);
     const uploadImage = useHoleMapUpload();
 
     const handlePickImage = async () => {
@@ -35,15 +40,46 @@ const HoleMap: FC = () => {
     };
 
     return (
-        <View>
-            <Text variant="headlineSmall">
-                Kuva {selectedRound}
-            </Text>
-            <Button mode="contained" onPress={handleTakePhoto}>
-                Press me
-            </Button>
+      <View style={{ flex: 1 }}>
+        <HoleSelector selectedHole={selectedRound} setSelectedHole={setSelectedRound} numberOfHoles={18} />
+        <View style={styles.textContainer}>
+          <Text variant="headlineSmall">
+            {t("screens.game.holeMap.noImageTitle")}
+          </Text>
+          <Text>{t("screens.game.holeMap.noImageText")}</Text>
         </View>
+        <Spacer size={30} />
+        <Image
+          source={require("assets/tee-sign.png")}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Spacer size={30} />
+        <View style={styles.buttons}>
+          <Button mode="contained" onPress={handleTakePhoto}>
+            {t("screens.game.holeMap.takePhoto")}
+          </Button>
+          <Button mode="contained-tonal" onPress={handlePickImage}>
+            {t("screens.game.holeMap.uploadFromGallery")}
+          </Button>
+        </View>
+      </View>
     );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: Dimensions.get('window').width,
+    aspectRatio: 1.5,
+    height: undefined
+  },
+  textContainer: {
+    padding: 10,
+  },
+  buttons: {
+    padding: 20,
+    gap: 8
+  }
+});
 
 export default HoleMap;
